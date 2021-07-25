46bfedb45be6acfb3913acc82718b7eeaeb83130
goodjwon <firstman9@protonmail.com>
2021-07-26 07:32:07 +0900

    날짜 미출력 오류 수정

--- myApiFiles/src/main/java/kr/my/files/MyApiFilesApplication.java
+++ myApiFiles/src/main/java/kr/my/files/MyApiFilesApplication.java
@@ -2,7 +2,9 @@ package kr.my.files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MyApiFilesApplication {
 
