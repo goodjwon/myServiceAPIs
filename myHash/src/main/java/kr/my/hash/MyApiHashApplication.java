362457a697e3f0f5bb43b66c21b61bd3e34724b0
goodjwon <firstman9@protonmail.com>
2022-02-18 08:17:18 +0900

    Project setting 오류 보정

---
+++ myHash/src/main/java/kr/my/hash/MyApiHashApplication.java
@@ -0,0 +1,12 @@
+package kr.my.hash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiHashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApiHashApplication.class, args);
+    }
+}
